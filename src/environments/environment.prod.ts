e6f6385590b4715ec306210001fcd4f619e5492f
Damon Garcia Garcia <damon99@hotmail.es>
2021-05-05 12:39:53 +0100

    Añadir Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDBndPwViIDKDk_mU0LwaK6ekOpSCpRZKk",
+    authDomain: "shopping-cart-daw2.firebaseapp.com",
+    projectId: "shopping-cart-daw2",
+    storageBucket: "shopping-cart-daw2.appspot.com",
+    messagingSenderId: "901462092480",
+    appId: "1:901462092480:web:fc744a77f6034369a05b25",
+    measurementId: "G-TN4DJQWLNW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDBndPwViIDKDk_mU0LwaK6ekOpSCpRZKk",
+    authDomain: "shopping-cart-daw2.firebaseapp.com",
+    projectId: "shopping-cart-daw2",
+    storageBucket: "shopping-cart-daw2.appspot.com",
+    messagingSenderId: "901462092480",
+    appId: "1:901462092480:web:fc744a77f6034369a05b25",
+    measurementId: "G-TN4DJQWLNW"
+  }
 };
 
 /*
